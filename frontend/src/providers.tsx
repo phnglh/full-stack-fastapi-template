172f066079c79b577c076cc5fe3204b16695cda8
phonglh <phonghngle@gmail.com>
2025-09-26 22:52:43 +0700

    fix: config root router

--- frontend/src/providers.tsx
+++ frontend/src/providers.tsx
@@ -1,3 +1,5 @@
+import { ChakraProvider } from "@chakra-ui/react"
+import { system } from "@chakra-ui/react/preset"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import type { ReactNode } from "react"
@@ -10,7 +12,9 @@ const queryClient = new QueryClient()
 export default function Providers({ children }: Props) {
   return (
     <QueryClientProvider client={queryClient}>
+      <ChakraProvider value={system}>
       {children}
+      </ChakraProvider>
       <ReactQueryDevtools />
     </QueryClientProvider>
   )
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,13 +1,25 @@
+import { Button } from "@/components/ui/button"
 import { createFileRoute } from "@tanstack/react-router"
+import { useEffect, useState } from "react"
 
 export const Route = createFileRoute("/")({
   component: Index,
 })
 
 function Index() {
+  const [value,setValue] = useState(false)
+  useEffect(()=> {
+    console.log('value',value)
+  },[value])
   return (
     <div className="p-2">
       <h3>Welcome Home!</h3>
+      <Button
+      size={"sm"}
+        onClick={()=> setValue((pre) => !pre)}
+      >
+        Click me
+      </Button>
     </div>
   )
 }
